7bd8bbcf463cc59d35d99972e0a756a7e4bd937a
Rosamelia Carioni <r.carioniporras@student.maastrichtuniversity.nl>
2021-09-17 19:31:22 +0200

    not working, fix it

--- pizza/menu/models.py
+++ pizza/menu/models.py
@@ -3,11 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Pizza(models.Model):
+    pizza_id = models.AutoField(primary_key=True)    
     pizza_name = models.CharField(max_length=200)
-    vegeterian = models.BooleanField(default=0)
-    price = models.IntegerField(default=10)
-        
-    def __str__(self):
+    def __str__(self): #function: looking up pizzas 
         return self.pizza_name
 
 
@@ -20,8 +18,8 @@ class Orders(models.Model):
     def __str__(self):
         return self.order_id
 
-class Order_Pizza(models.Model):
-    pizza_id = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+#class Order_Pizza(models.Model):
+ #   pizza_id = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+  #  order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
 
 
