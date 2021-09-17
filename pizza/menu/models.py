25492c47111be126c13a0a06dd4df9ef4c807245
MischaRauch <mischa1297@gmx.de>
2021-09-17 20:55:14 +0200

    fixed pk nameing

--- pizza/menu/models.py
+++ pizza/menu/models.py
@@ -10,16 +10,18 @@ class Pizza(models.Model):
 
 
 class Orders(models.Model):
+    order_id = models.BigAutoField(primary_key=True)
     customer_id = models.IntegerField(default=0) # must be a FK later
-    order_id = models.IntegerField(default=0)
     order_time = models.DateTimeField('orderedTime')
     order_status = models.CharField(max_length=200)
 
     def __str__(self):
         return self.order_id
 
-#class Order_Pizza(models.Model):
- #   pizza_id = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-  #  order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+class Order_Pizza(models.Model):
+    pizza_id = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
 
 
+
+    
