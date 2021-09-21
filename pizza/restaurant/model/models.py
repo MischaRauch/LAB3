9d32c722326f9332d8e3b5aa395a42e74099e808
MischaRauch <mischa1297@gmx.de>
2021-09-21 22:58:02 +0200

    newstuff

--- pizza/restaurant/model/models.py
+++ pizza/restaurant/model/models.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 class pizza(models.Model):
     pizza_id = models.AutoField(primary_key=True)    
-    pizza_name = models.VARCHAR(200) 
+    pizza_name = models.CharField(max_length=100) 
 
     def __str__(self): #function: looking up pizzas         
         return self.pizza_name        
@@ -16,7 +16,7 @@ class pizza(models.Model):
          
 class topping(models.Model):
     topping_id = models.AutoField(primary_key=True)    
-    topping_name = models.VARCHAR(200)
+    topping_name = models.CharField(max_length=100)
     topping_price = models.FloatField() 
     vegeterian = models.BooleanField()
 
@@ -33,7 +33,7 @@ class pizza_toppings(models.Model):
 
 class drink(models.Model):
     drink_id = models.AutoField(primary_key=True)    
-    drink_name = models.VARCHAR(200)
+    drink_name = models.CharField(max_length=100)
     drink_price = models.FloatField() 
     
     def __str__(self): 
@@ -41,59 +41,37 @@ class drink(models.Model):
 
 class desert(models.Model):
     desert_id = models.AutoField(primary_key=True)    
-    desert_name = models.VARCHAR(200)
+    desert_name = models.CharField(max_length=100)
     desert_price = models.FloatField() 
     def __str__(self): 
         return self.desert_id
 
 class address(models.Model):
     address_id = models.AutoField(primary_key=True)    
-    postal_code = models.VARCHAR(200)
-    country = models.VARCHAR(200)
-    street = models.VARCHAR(200)
+    postal_code = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
     house_number = models.IntegerField(default=0) 
-    city = models.VARCHAR(200)
+    city = models.CharField(max_length=100)
     def __str__(self): 
         return self.address_id
 
 class customer(models.Model):
     customer_id = models.AutoField(primary_key=True)    
-    first_name = models.VARCHAR(200)
-    last_name = models.VARCHAR(200)
-    email_address = models.VARCHAR(200)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.CharField(max_length=100)
     phone_number = models.IntegerField(default=0) 
     discount_available = models.BooleanField(default=False)
     address_id = models.ForeignKey(address, on_delete= models.CASCADE) #FK
     def __str__(self): 
         return self.customer_id
 
-
-class orders(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    customer_id = models.ForeignKey(customer, on_delete=models.CASCADE)    #FK 
-    total_price = models.FloatField(default=0) #calculated from drink, desert and pizza 
-    total_discount= models.FloatField(default=0) # get boolean from costumer 
-    order_time = models.DateTimeField('orderedTime')
-
-    def __str__(self):
-        return self.order_id
-
-class order_item(models.Model):
-    order_item_id = models.AutoField(primary_key=True)    
-    quantity = models.IntegerField(default=0) 
-    pizza_id = models.ForeignKey(pizza, on_delete=models.CASCADE)    #FK
-    drink_id = models.ForeignKey(drink, on_delete=models.CASCADE)    #FK 
-    desert_id = models.ForeignKey(desert, on_delete=models.CASCADE)    #FK
-    order_id = models.ForeignKey(orders, on_delete=models.CASCADE)    #FK
-    def __str__(self): 
-        return self.menu_item_id
-
-
 class employee(models.Model):
     employee_id = models.AutoField(primary_key=True)    
-    first_name = models.VARCHAR(200)
-    last_name = models.VARCHAR(200)
-    area_code = models.VARCHAR(200)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    area_code = models.CharField(max_length=100)
     
     def __str__(self): 
         return self.employee_id
@@ -113,7 +91,33 @@ class delivery(models.Model):
     time_when_employee_left = models.DateTimeField('orderedTime')
     
     def __str__(self): 
-        return self.employee_id
+        return self.delivery_id
+
+class orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    customer_id = models.ForeignKey(customer, on_delete=models.CASCADE)    #FK 
+    total_price = models.FloatField(default=0) #calculated from drink, desert and pizza 
+    total_discount= models.FloatField(default=0) # get boolean from costumer 
+    order_time = models.DateTimeField('orderedTime')
+    delivery_id = models.ForeignKey(delivery, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.order_id
+
+class order_item(models.Model):
+    order_item_id = models.AutoField(primary_key=True)    
+    quantity = models.IntegerField(default=0) 
+    pizza_id = models.ForeignKey(pizza, on_delete=models.CASCADE)    #FK
+    drink_id = models.ForeignKey(drink, on_delete=models.CASCADE)    #FK 
+    desert_id = models.ForeignKey(desert, on_delete=models.CASCADE)    #FK
+    order_id = models.ForeignKey(orders, on_delete=models.CASCADE)    #FK
+    def __str__(self): 
+        return self.order_item_id
+
+
+
+
+
 
 
     
--- pizza/restaurant/view/urls.py
+++ pizza/restaurant/view/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pizza_id>/', views.detail, name='detail'),
     path('listpizza', views.listpizzas, name='gallery'),
-    path('query', views.query)
+#    path('query', views.query)
 ]
