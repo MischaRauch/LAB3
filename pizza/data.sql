d2b0f1941f0b6cb500544bf4fc25f16ec1ae4d49
MischaRauch <mischa1297@gmx.de>
2021-09-21 10:07:35 +0200

    data

---
+++ pizza/data.sql
@@ -0,0 +1,18 @@
+-- SOURCE C:/path/data.sql;
+
+
+USE pizzadatabase;
+
+INSERT INTO menu_pizza VALUES 
+    (1, 'Margaritha'),
+    (2, 'Meat Lovers'),
+    (3, 'Hawaii')
+; 
+INSERT INTO menu_topping VALUES 
+    (1, 'cheese', 2, true),
+    (2, 'ham', 4, false)
+;
+INSERT INTO menu_drink VALUES 
+    (1, 'CocaCola', 3)
+;
+
