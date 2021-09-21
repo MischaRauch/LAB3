334cf06b72ca249ded8d5838ea53149d24155d6f
Rosamelia Carioni <r.carioniporras@student.maastrichtuniversity.nl>
2021-09-21 23:35:50 +0200

    queries on point

--- pizza/restaurant/view/views.py
+++ pizza/restaurant/view/views.py
@@ -1,7 +1,7 @@
+
 from django.shortcuts import get_object_or_404, render
-
+from restaurant.controller import queries 
 from restaurant.model.models import pizza
-
 
 def index(request):
     pizza_list = pizza.objects.order_by('-pizza_id')
@@ -10,12 +10,10 @@ def index(request):
     
 
 def detail(request, pizza_id):
+    selected_pizza2=  queries.get_pizzas(pizza_id)  
     selected_pizza = get_object_or_404(pizza, pk=pizza_id)
-    sel = pizza.objects.get(pk=pizza_id)
-    #seol = pizza.objects.filter(pizza_id= 2)
-    print(sel)
-    #pizza = get_object_or_404(pizza_id, pk)
-    get_pizza_toppings(request)
+    print ('im here          ' )
+    print (selected_pizza2)
     return render(request, 'restaurant/selectPizzaToppings.html', {'pizza': selected_pizza})
 
 def listpizzas(request):
